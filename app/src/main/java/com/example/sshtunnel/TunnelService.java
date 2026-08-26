90755249adf5a2b0474e18e47c181a941a57eec8
sonofstrange <strange.z2tablet@gmail.com>
2026-08-27 00:57:33 +0700

    Release 1.37-beta.5: Prioritize hardware AES-GCM and ChaCha20 with 8MB window and 128KB stream buffers

--- app/src/main/java/com/example/sshtunnel/TunnelService.java
+++ app/src/main/java/com/example/sshtunnel/TunnelService.java
@@ -543,9 +543,9 @@ public class TunnelService extends Service {
         result.setConfig("compression.c2s", "none");
         result.setConfig("compression.s2c", "none");
         result.setConfig("cipher.c2s",
-                "aes128-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com,chacha20-poly1305@openssh.com,aes256-ctr");
+                "chacha20-poly1305@openssh.com,aes128-gcm@openssh.com,aes256-gcm@openssh.com,aes128-ctr,aes256-ctr");
         result.setConfig("cipher.s2c",
-                "aes128-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com,chacha20-poly1305@openssh.com,aes256-ctr");
+                "aes128-gcm@openssh.com,aes128-ctr,chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes256-ctr");
         result.setConfig("mac.c2s",
                 "hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha2-256,hmac-sha2-512");
         result.setConfig("mac.s2c",
